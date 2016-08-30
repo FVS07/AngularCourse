de1690d5b3e9815d4d3136afb0c7cab79cac8c30
FVS07 <fer_.venzor@hotmail.com>
2016-08-30 12:56:53 -0600

    a

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+(function  () {
+
+	var gem = {
+		name: 'Azurite',
+		price: '2.95',
+		description: 'lorem ipsum dolor sit amet,'
+
+	};
 var app = angular.module('gemStore', []);
+	// body...
+	app.controller('StoreController', function  () {
+		// body...
+	})
+})();
