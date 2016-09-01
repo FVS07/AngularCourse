b562556eeb110ee5b311c2f07568eb22c1f6469c
FVS07 <fer_.venzor@hotmail.com>
2016-09-01 12:58:21 -0600

    a

--- js/app.js
+++ js/app.js
@@ -6,21 +6,36 @@
 			price: '2.95',
 			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
 			canPurchase: false,
-			soldOut: true
+			soldOut: true,
+			images:
+
+					{id:0, url:'Documentos/tec2/img/gem-01.gif'}
+					{id:1, url:'Documentos/tec2/img/gem-02.gif'}
+					{id:2, url:'Documentos/tec2/img/gem-03.gif'}
 		},
 		{
 			name: 'Bloodstone',
 			price: '5.95',
 			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
 			canPurchase: false,
-			soldOut: true
+			soldOut: true,
+			images:
+
+					{id:0, url:'Documentos/tec2/img/gem-04.gif'}
+					{id:1, url:'Documentos/tec2/img/gem-05.gif'}
+					{id:2, url:'Documentos/tec2/img/gem-06.gif'}
 		},
 		{
 			name: 'Zircon',
 			price: '3.95',
 			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
 			canPurchase: false,
-			soldOut: true
+			soldOut: true,
+			images:
+
+					{id:0, url:'Documentos/tec2/img/gem-07.gif'}
+					{id:1, url:'Documentos/tec2/img/gem-08.gif'}
+					{id:2, url:'Documentos/tec2/img/gem-09.gif'}
 		}	
 	];
 
